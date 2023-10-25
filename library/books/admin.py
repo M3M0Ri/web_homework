b6909624c5e14511d071db33bd4e793100f255c4
mahdi-gasemfar <96542298+mahdi-gasemfar@users.noreply.github.com>
2023-10-25 15:52:40 +0330

    commit N-04 update admin.py

--- library/books/admin.py
+++ library/books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Shelf, Author, Book
 
-# Register your models here.
+admin.site.register(Shelf)
+admin.site.register(Author)
+admin.site.register(Book)
